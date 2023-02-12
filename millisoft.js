f24d4acb6a9c10c082c7c9ed75e30b1aa0e6e81c
Ziya Akhundov <95044753+ZiyaAkhundov@users.noreply.github.com>
2023-02-13 00:45:38 +0400

    Update millisoft.js

--- millisoft.js
+++ millisoft.js
@@ -1,13 +1,13 @@
-function lock(){
+function lock(orientation){
   let de=document.documentElement;
   if(de.requestFullscreen){de.requestFullscreen()}
   else if(de.mozRequestFullscreen){de.mozRequestFullscreen()}
   else if(de.webkitRequestFullscreen){de.webkitRequestFullscreen()}
   else if(de.requestFullscreen){de.requestFullscreen()}
   else if(de.msRequestFullscreen){de.msRequestFullscreen()}
-  screen.orientation.lock();
+  screen.orientation.lock(orientation);
 }
-lock();
+lock('portrait')
 $(window).ready(function() {
     $(window).scroll(function() {
       var scroll = $(window).scrollTop();
